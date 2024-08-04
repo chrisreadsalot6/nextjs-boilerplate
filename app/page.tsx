754d243d56c40fa810a74d49a4adee0712aa871c
chrisreadsalot6 <rcreadii@gmail.com>
2024-08-03 19:39:42 -0500

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
 					rel="noopener noreferrer"
 				>
 					<h2 className="mb-3 text-2xl font-semibold">
-						Does My Account Have Enough?{' '}
+						Do I Have Enough In My Account?{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
@@ -83,31 +83,13 @@ export default function Home() {
 					rel="noopener noreferrer"
 				>
 					<h2 className="mb-3 text-2xl font-semibold">
-						How Much Do I Spend?{' '}
+						How Much Do I Spend A Month?{' '}
 						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
 							-&gt;
 						</span>
 					</h2>
 					<p className="m-0 max-w-[30ch] text-sm opacity-50">
 						Explore starter templates for Next.js.
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className="mb-3 text-2xl font-semibold">
-						Deploy{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-						Instantly deploy your Next.js site to a shareable URL
-						with Vercel.
 					</p>
 				</a>
 			</div>
