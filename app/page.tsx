2683230729beacbaea623f4d2123118f3fa1813a
chrisreadsalot6 <rcreadii@gmail.com>
2024-08-04 17:15:25 -0500

    adding google cal API connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
 import Image from 'next/image';
+import { google } from 'googleapis';
 
 export default function Home() {
+	const oauth2Client = new google.auth.OAuth2(
+		YOUR_CLIENT_ID,
+		YOUR_CLIENT_SECRET,
+		YOUR_REDIRECT_URL
+	);
+
+	// generate a url that asks permissions for Blogger and Google Calendar scopes
+	const scopes = [
+		'https://www.googleapis.com/auth/blogger',
+		'https://www.googleapis.com/auth/calendar',
+	];
+
+	const url = oauth2Client.generateAuthUrl({
+		// 'online' (default) or 'offline' (gets refresh_token)
+		access_type: 'offline',
+
+		// If you only need one scope, you can pass it as a string
+		scope: scopes,
+	});
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
