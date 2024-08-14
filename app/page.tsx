af8041784c2bcfd091fbdb19c19e30dbdd801a3b
chrisreadsalot6 <rcreadii@gmail.com>
2024-08-13 19:22:04 -0500

    IN PROGRESS - add google auth

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,8 @@ export default async function Home() {
 		return <p>Access Denied</p>;
 	}
 
+	console.log({ session });
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
